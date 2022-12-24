77d6d47d62b96ccb6a8ba548a2224858d3e67d38
srilasyakompella <srilasyakompella@gmail.com>
2022-12-24 18:07:15 +0530

    addded checkboxes

--- src/loanApp.py
+++ src/loanApp.py
@@ -6,18 +6,54 @@ import streamlit as st
 from PIL import Image
   
 # loading in the model to predict on the data
-pickle_in = open(r"C:\Users\srila\classifier.pkl", 'rb')
+pickle_in = open(r".\classifier.pkl", 'rb')
 classifier = pickle.load(pickle_in)
 
 
-def prediction(Gender, Married, Dependents, Education, Self_Employed, ApplicantIncome, CoapplicantIncome, LoanAmount,
-        Loan_Amount_Term, Credit_History, Property_Area):  
+def prediction(Attributes):  
    
     prediction = classifier.predict(
-        [[Gender, Married, Dependents, Education, Self_Employed, ApplicantIncome, CoapplicantIncome, LoanAmount,
-        Loan_Amount_Term, Credit_History, Property_Area]])
+        [Attributes])
     print(prediction)
     return prediction
+
+def toCategoricalVar(Gender, Married, Dependents, Education, Self_Employed, ApplicantIncome, CoapplicantIncome, LoanAmount,
+    Loan_Amount_Term, Credit_History, Property_Area):
+    if Gender == "Male":
+        Gender = 0
+    else:
+        Gender = 1
+
+
+    if Married == "No":
+        Married = 0
+    else:
+        Married = 1
+
+
+    if Education == "Graduate":
+        Education = 0
+    else:
+        Education = 1
+
+
+    if Self_Employed == "No":
+        Self_Employed = 0
+    else:
+        Self_Employed = 1
+
+
+    if Property_Area == "Urban":
+        Property_Area = 2
+    elif Property_Area == "Rural":
+        Property_Area = 0
+    else:
+        Property_Area = 1
+
+
+    return [Gender, Married, Dependents, Education, Self_Employed, ApplicantIncome, CoapplicantIncome, LoanAmount,
+        Loan_Amount_Term, Credit_History, Property_Area]
+        
  
 def main():
       # giving the webpage a title
@@ -37,24 +73,41 @@ def main():
       
     # the following lines create text boxes in which the user can enter 
     # the data required to make the prediction
-    Gender = st.text_input("Gender", "Type Here")
-    Married = st.text_input("Married", "Type Here")
+    
+    Gender = st.selectbox(
+    'Gender',
+    ('Male', 'Female'))
+    Married = st.selectbox("Married", 
+    ('Yes' , 'No'))
     Dependents = st.text_input("Dependents", "Type Here")
-    Education = st.text_input("Education", "Type Here")
-    Self_Employed = st.text_input("Self_Employed", "Type Here")
+    Education = st.selectbox(
+    'Education',
+    ('Graduate', 'Not Graduate'))
+    Self_Employed = st.selectbox(
+    'Self Employed',
+    ('Yes', 'No'))
     ApplicantIncome = st.text_input("ApplicantIncome", "Type Here")
     CoapplicantIncome = st.text_input("CoapplicantIncome", "Type Here")
     LoanAmount = st.text_input("LoanAmount", "Type Here")
     Loan_Amount_Term = st.text_input("Loan_Amount_Term", "Type Here")
     Credit_History = st.text_input("Credit_History", "Type Here")
-    Property_Area = st.text_input("Property_Area", "Type Here")
+    Property_Area = st.selectbox(
+    'Property Area',
+    ('Urban', 'Rural','Semi-Urban'))
     
+    Attributes = toCategoricalVar(Gender, Married, Dependents, Education, Self_Employed, ApplicantIncome, CoapplicantIncome, LoanAmount,
+        Loan_Amount_Term, Credit_History, Property_Area)
+
     
     result =""
+    ans = ""
     if st.button("Predict"):
-        result = prediction(Gender, Married, Dependents, Education, Self_Employed, ApplicantIncome, CoapplicantIncome, LoanAmount,
-        Loan_Amount_Term, Credit_History, Property_Area)
-    st.success('The output is {}'.format(result))
+        result = prediction(Attributes)
+    if result == 1:
+        ans = "Loan approval is possible"
+    else:
+        ans = "Loan cannot be granted"
+    st.success(ans)
      
 if __name__=='__main__':
     main()
